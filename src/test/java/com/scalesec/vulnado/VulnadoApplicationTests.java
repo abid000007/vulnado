48ed3433e6be050c21d8206b83a7300b7b5011fc
abid000007 <123622419+abid000007@users.noreply.github.com>
2024-12-03 17:48:16 +0500

    Update VulnadoApplicationTests.java

--- src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java
+++ src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java
@@ -1,17 +1,12 @@
-package com.scalesec.vulnado;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class VulnadoApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.headers()
+            .frameOptions().sameOrigin()  // Prevent clickjacking
+            .and()
+            .xssProtection().block(true) // Enable XSS protection
+            .and()
+            .contentSecurityPolicy("default-src 'self';"); // Define CSP
+    }
 }
-
